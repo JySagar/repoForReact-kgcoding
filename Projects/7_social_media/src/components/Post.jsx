bdd059cbd8e04b546a4b7cff856920cda7ecb655
JySagar <sagarjyotidas0@gmail.com>
2025-07-21 02:18:00 +0530

    made the cards show the details like titles, body, hastags, reaction.

--- Projects/7_social_media/src/components/Post.jsx
+++ Projects/7_social_media/src/components/Post.jsx
@@ -1,15 +1,21 @@
-const Post = () => {
+const Post = ({ postPassed }) => {
   return (
-    <div className="card" style={{ width: "18rem" }}>
+    <div className="card post-card" style={{ width: "30rem" }}>
       <div className="card-body">
-        <h5 className="card-title">Card title</h5>
-        <p className="card-text">
-          Some quick example text to build on the card title and make up the
-          bulk of the card’s content.
-        </p>
-        <a href="#" className="btn btn-primary">
-          Go somewhere
-        </a>
+        <h5 className="card-title">
+          {postPassed.title}
+          <span class="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-danger">
+            {postPassed.reactions}
+            {/* <span class="visually-hidden">unread messages</span> */}
+          </span>
+        </h5>
+        <p className="card-text">{postPassed.body}</p>
+        {postPassed.tags.map((tag) => (
+          <span class="badge text-bg-primary hastag">{tag}</span>
+        ))}
+        <div class="alert alert-success reactions" role="alert">
+          A simple success alert—check it out!
+        </div>
       </div>
     </div>
   );
---
+++ Projects/7_social_media/src/store/post-list-store.jsx
@@ -0,0 +1,56 @@
+import { useReducer } from "react";
+import { createContext } from "react";
+
+export const PostList = createContext({
+  postList: [],
+  addPost: () => {},
+  deletePost: () => {},
+});
+
+const postListReducer = (currentPostList, action) => {
+  return currentPostList;
+};
+
+const PostListProvider = ({ children }) => {
+  const [postListFP, dispatchPostList] = useReducer(
+    postListReducer,
+    DEFAULT_POST_LIST
+  );
+
+  const addPostFP = () => {};
+
+  const deletePostFP = () => {};
+
+  return (
+    <PostList.Provider
+      value={{
+        postList: postListFP,
+        addPost: addPostFP,
+        deletePost: deletePostFP,
+      }}
+    >
+      {children}
+    </PostList.Provider>
+  );
+};
+
+const DEFAULT_POST_LIST = [
+  {
+    id: "1",
+    title: "Going to Mumbai",
+    body: "Hi friends i am going to Mumbai for my vacations. Hope to enjoy a lot.",
+    reactions: 2,
+    userID: "user-9",
+    tags: ["vacation", "Mumbai", "Enjoying"],
+  },
+  {
+    id: "2",
+    title: "Pass ho bhai",
+    body: "4 saal ki masti ka baad bhi ho gaye hain paas. Hard to believe",
+    reactions: 15,
+    userID: "user-12",
+    tags: ["Graduating", "Unbelievable"],
+  },
+];
+
+export default PostListProvider;
