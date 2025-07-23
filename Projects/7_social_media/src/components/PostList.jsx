889e5c8ecc36e02930460bb00b0107f756f75b18
JySagar <sagarjyotidas0@gmail.com>
2025-07-24 02:36:54 +0530

    added useState so that re-render doesn't happens

--- Projects/7_social_media/src/components/PostList.jsx
+++ Projects/7_social_media/src/components/PostList.jsx
@@ -1,22 +1,28 @@
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import Post from "./Post";
 import { PostList as PostListData } from "../store/post-list-store";
 import WelcomeMessage from "./WelcomeMessage";
 
 const PostList = () => {
   const { postList, addInitialPosts } = useContext(PostListData);
-  const handleGetPostsClicked = () => {
+
+  const [datafetched, setDataFetched] = useState(false);
+
+  if (!datafetched) {
     fetch("https://dummyjson.com/posts")
       .then((res) => res.json())
       // .then(console.log);
       .then((data) => {
         addInitialPosts(data.posts);
       });
-  };
+    setDataFetched(true);
+  }
+
+  // const handleGetPostsClicked = () => {};
   return (
     <>
       {postList.length === 0 && (
-        <WelcomeMessage getPostsClickPassed={handleGetPostsClicked} />
+        <WelcomeMessage /*getPostsClickPassed={handleGetPostsClicked}*/ />
       )}
       {postList.map((post) => (
         // console.log(post);
--- Projects/7_social_media/src/components/WelcomeMessage.jsx
+++ Projects/7_social_media/src/components/WelcomeMessage.jsx
@@ -1,14 +1,18 @@
-const WelcomeMessage = ({ getPostsClickPassed }) => {
+const WelcomeMessage = (
+  {
+    /* getPostsClickPassed */
+  }
+) => {
   return (
     <center className="welcome-message">
       <h1>There are not posts</h1>
-      <button
+      {/* <button
         onClick={getPostsClickPassed}
         type="button"
         className="btn btn-primary"
       >
         Get Posts From Server
-      </button>
+      </button> */}
     </center>
   );
 };
