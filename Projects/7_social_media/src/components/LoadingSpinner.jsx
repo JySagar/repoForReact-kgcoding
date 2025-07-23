3e910e753f7dd57875b27078274e272a3d770a6b
JySagar <sagarjyotidas0@gmail.com>
2025-07-24 03:33:32 +0530

    now when fetching welcome and Post not running

---
+++ Projects/7_social_media/src/components/LoadingSpinner.jsx
@@ -0,0 +1,5 @@
+const LoadingSpinner = () => {
+  return <h1>LOADING...</h1>;
+};
+
+export default LoadingSpinner;
--- Projects/7_social_media/src/components/PostList.jsx
+++ Projects/7_social_media/src/components/PostList.jsx
@@ -2,18 +2,20 @@ import { useContext, useEffect, useState } from "react";
 import Post from "./Post";
 import { PostList as PostListData } from "../store/post-list-store";
 import WelcomeMessage from "./WelcomeMessage";
+import LoadingSpinner from "./LoadingSpinner";
 
 const PostList = () => {
   const { postList, addInitialPosts } = useContext(PostListData);
   const [fetching, setFetching] = useState(false);
 
   useEffect(() => {
+    // The below true happens just before the fetching of data where we will start the loading icon.
     setFetching(true);
     fetch("https://dummyjson.com/posts")
       .then((res) => res.json())
-      // .then(console.log);
       .then((data) => {
         addInitialPosts(data.posts);
+        // The below false happens after fetching is done i.e. posts are here so we will stop the loading icon.
         setFetching(false);
       });
   }, []);
@@ -21,13 +23,15 @@ const PostList = () => {
   // const handleGetPostsClicked = () => {};
   return (
     <>
-      {postList.length === 0 && (
+      {fetching && <LoadingSpinner />}
+      {!fetching && postList.length === 0 && (
         <WelcomeMessage /*getPostsClickPassed={handleGetPostsClicked}*/ />
       )}
-      {postList.map((post) => (
-        // console.log(post);
-        <Post key={post.id} postPassed={post} />
-      ))}
+      {!fetching &&
+        postList.map((post) => (
+          // console.log(post);
+          <Post key={post.id} postPassed={post} />
+        ))}
     </>
   );
 };
