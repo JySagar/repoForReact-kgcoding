e7fa7e88f615e79e086a7bd4099963925dd70483
JySagar <sagarjyotidas0@gmail.com>
2025-07-26 18:24:58 +0530

    useEffect is applied in this bharat clock

--- Projects/8_bharat_clock_version_2/8-bharat-clock-version-2/src/components/CurrentTime.jsx
+++ Projects/8_bharat_clock_version_2/8-bharat-clock-version-2/src/components/CurrentTime.jsx
@@ -1,5 +1,19 @@
+import { useEffect, useState } from "react";
+
 function CurrentTime() {
-  let time = new Date();
+  // let time = new Date();
+
+  const [time, setTime] = useState(new Date());
+
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      setTime(new Date());
+    }, 1000);
+
+    return () => {
+      clearInterval(intervalId);
+    };
+  }, []);
 
   return (
     <p className="lead">
@@ -12,3 +26,5 @@ function CurrentTime() {
 export default CurrentTime;
 
 // 9863486790
+
+// Here all the things that are summed up to this much and all the things that can be done is done till now.
