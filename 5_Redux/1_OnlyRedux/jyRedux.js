d8e7abf98ad8fcf1bc8d63e03c3070d846d84e33
JySagar <sagarjyotidas0@gmail.com>
2025-08-14 01:58:35 +0530

    redux on progress

--- 5_Redux/1_OnlyRedux/jyRedux.js
+++ 5_Redux/1_OnlyRedux/jyRedux.js
@@ -1,1 +1,27 @@
 // To make a node project first: npm init -y
+// Now install redux package : npm install redux
+
+const redux = require("redux");
+
+// Now to make reducer:
+
+const reducerVar = (storeParameter, actionParameter) => {
+  return {counter : storeParameter.counter + 1};
+};
+
+// Now to make Store:
+
+const storeVar = redux.createStore(reducerVar);
+
+// Now to make subscriber:
+
+const subscriberVar = () => {
+  const stateVar = storeVar.getState();
+  console.log(stateVar);
+};
+
+storeVar.subscribe(subscriberVar);
+
+// Now to make action:
+
+storeVar.dispatch({type: })
