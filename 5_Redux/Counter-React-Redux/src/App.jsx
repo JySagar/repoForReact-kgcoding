46bc269e6a04be517f23f6c63d09a2f46fec9b92
JySagar <sagarjyotidas0@gmail.com>
2025-08-14 18:47:09 +0530

    just

--- 5_Redux/Counter-React-Redux/src/App.jsx
+++ 5_Redux/Counter-React-Redux/src/App.jsx
@@ -1,30 +1,31 @@
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
+import Header from "./components/Header";
 
 function App() {
   return (
     <>
-      <div class="px-4 py-5 my-5 text-center">
-        {" "}
-        <h1 class="display-5 fw-bold text-body-emphasis">Centered hero</h1>{" "}
-        <div class="col-lg-6 mx-auto">
-          {" "}
-          <p class="lead mb-4">
+      <div className="px-4 py-5 my-5 text-center">
+        <Header />
+        <div className="col-lg-6 mx-auto">
+          <p className="lead mb-4">
             Quickly design and customize responsive mobile-first sites with
-            Bootstrap, the world’s most popular front-end open source toolkit,
+            Bootstrap, the world's most popular front-end open source toolkit,
             featuring Sass variables and mixins, responsive grid system,
             extensive prebuilt components, and powerful JavaScript plugins.
-          </p>{" "}
-          <div class="d-grid gap-2 d-sm-flex justify-content-sm-center">
-            {" "}
-            <button type="button" class="btn btn-primary btn-lg px-4 gap-3">
+          </p>
+          <div className="d-grid gap-2 d-sm-flex justify-content-sm-center">
+            <button type="button" className="btn btn-primary btn-lg px-4 gap-3">
               Primary button
-            </button>{" "}
-            <button type="button" class="btn btn-outline-secondary btn-lg px-4">
+            </button>
+            <button
+              type="button"
+              className="btn btn-outline-secondary btn-lg px-4"
+            >
               Secondary
-            </button>{" "}
-          </div>{" "}
-        </div>{" "}
+            </button>
+          </div>
+        </div>
       </div>
     </>
   );
