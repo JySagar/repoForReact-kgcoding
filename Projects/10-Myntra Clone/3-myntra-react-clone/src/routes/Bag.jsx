b907992a9fb9936481446a2091a18bb8bcbcaa56
JySagar <sagarjyotidas0@gmail.com>
2025-08-22 15:04:26 +0530

    added react router

---
+++ Projects/10-Myntra Clone/3-myntra-react-clone/src/routes/Bag.jsx
@@ -0,0 +1,15 @@
+import Footer from "../components/Footer";
+import Header from "../components/Header";
+
+const Bag = () => {
+  return (
+    <main>
+      <div class="bag-page">
+        <div class="bag-items-container"></div>
+        <div class="bag-summary"></div>
+      </div>
+    </main>
+  );
+};
+
+export default Bag;
